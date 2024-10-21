c331b3f9b6121b2a65a424f723f46b8a6258ce5f
devanand-jrx <devanand.ravindran@edstem.com>
2024-10-21 15:19:39 +0530

    paragraph

--- Task3/GradeManager.cs
+++ Task3/GradeManager.cs
@@ -1,90 +1,90 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace PracticalEvaluationDevanand.Task3
-{
-    internal class GradeManager
-    {
-        static void Main(string[] args)
-        {
-            int size = 5;
-            double[] grades = new double[size];
-            int gradeCount = 0;
-            Console.WriteLine("Enter marks between 0 and 100 / type 'exit' to finish:");
-            while (true)
-            {
-                if (gradeCount == grades.Length)
-                {
-                    Array.Resize(ref grades, grades.Length + 5);
-                }
-                string input = Console.ReadLine();
-                if (input.ToLower() == "exit")
-                {
-                    break;
-                }
-                double grade;
-                if (double.TryParse(input, out grade) && grade >= 0 && grade <= 100)
-                {
-                    grades[gradeCount] = grade;
-                    gradeCount++;
-                }
-                else
-                {
-                    Console.WriteLine("Invalid input. Please enter a grade between 0 and 100 or type 'exit' to finish:");
-                }
-            }
-            if (gradeCount == 0)
-            {
-                Console.WriteLine("No grades entered.");
-                return;
-            }
+//namespace PracticalEvaluationDevanand.Task3
+//{
+//    internal class GradeManager
+//    {
+//        static void Main(string[] args)
+//        {
+//            int size = 5;
+//            double[] grades = new double[size];
+//            int gradeCount = 0;
+//            Console.WriteLine("Enter marks between 0 and 100 / type 'exit' to finish:");
+//            while (true)
+//            {
+//                if (gradeCount == grades.Length)
+//                {
+//                    Array.Resize(ref grades, grades.Length + 5);
+//                }
+//                string input = Console.ReadLine();
+//                if (input.ToLower() == "exit")
+//                {
+//                    break;
+//                }
+//                double grade;
+//                if (double.TryParse(input, out grade) && grade >= 0 && grade <= 100)
+//                {
+//                    grades[gradeCount] = grade;
+//                    gradeCount++;
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Invalid input. Please enter a grade between 0 and 100 or type 'exit' to finish:");
+//                }
+//            }
+//            if (gradeCount == 0)
+//            {
+//                Console.WriteLine("No grades entered");
+//                return;
+//            }
 
-            double sum = 0;
-            for (int i = 0; i < gradeCount; i++)
-            {
-                sum += grades[i];
-            }
-            double average = sum / gradeCount;
-            Console.WriteLine($"Class average: {average}");
+//            double sum = 0;
+//            for (int i = 0; i < gradeCount; i++)
+//            {
+//                sum += grades[i];
+//            }
+//            double average = sum / gradeCount;
+//            Console.WriteLine($"Class average: {average}");
 
-            double highest = grades[0];
-            double lowest = grades[0];
-            for (int i = 1; i < gradeCount; i++)
-            {
-                if (grades[i] > highest)
-                {
-                    highest = grades[i];
-                }
-                if (grades[i] < lowest)
-                {
-                    lowest = grades[i];
-                }
-            }
-            Console.WriteLine($"Highest grade: {highest}");
-            Console.WriteLine($"Lowest grade: {lowest}");
+//            double highest = grades[0];
+//            double lowest = grades[0];
+//            for (int i = 1; i < gradeCount; i++)
+//            {
+//                if (grades[i] > highest)
+//                {
+//                    highest = grades[i];
+//                }
+//                if (grades[i] < lowest)
+//                {
+//                    lowest = grades[i];
+//                }
+//            }
+//            Console.WriteLine($"Highest grade: {highest}");
+//            Console.WriteLine($"Lowest grade: {lowest}");
             
-            int passCount = 0;
-            int failCount = 0;
-            for (int i = 0; i < gradeCount; i++)
-            {
-                if (grades[i] >= 50)
-                {
-                    passCount++;
-                }
-                else
-                {
-                    failCount++;
-                }
-            }
-            Console.WriteLine($"Number of students passed: {passCount}");
-            Console.WriteLine($"Number of students failed: {failCount}");
-        }
+//            int passCount = 0;
+//            int failCount = 0;
+//            for (int i = 0; i < gradeCount; i++)
+//            {
+//                if (grades[i] >= 50)
+//                {
+//                    passCount++;
+//                }
+//                else
+//                {
+//                    failCount++;
+//                }
+//            }
+//            Console.WriteLine($"Number of students passed: {passCount}");
+//            Console.WriteLine($"Number of students failed: {failCount}");
+//        }
 
-    }
+//    }
         
 
     
-}
+//}
---
+++ Task4/TextProcessing.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalEvaluationDevanand.Task4
+{
+    public class TextProcessing
+    {
+        static string SummarizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+
+            var words = text.Split(' ');
+            var totalCharacters = 0;
+            var summaryWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                summaryWords.Add(word);
+                totalCharacters += word.Length +1;
+                if (totalCharacters > maxLength)
+                    break;
+            }
+            return string.Join(" ", summaryWords) + "...";
+        }
+
+        static int CountWords(string text)
+        {
+            string[] words = text.Split(' ');
+            return words.Length;
+        }
+        static int CountSentences(string text)
+        {
+            string[] sentences = text.Split(new char[] { '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            return sentences.Length;
+        }
+        static int CountCharacters(string text)
+        {
+            return text.Length;
+        }
+        static string FindLongestWord(string text)
+        {
+            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            string longestWord = string.Empty;
+            foreach (string word in words)
+            {
+                if (word.Length > longestWord.Length)
+                {
+                    longestWord = word;
+                }
+            }
+            return longestWord;
+        }
+        static string ReverseWords(string text)
+        {
+            StringBuilder reversedText = new StringBuilder();
+            string[] words = text.Split(new char[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.None);
+            foreach (string word in words)
+            {
+                StringBuilder reversedWord = new StringBuilder();
+                for (int i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord.Append(word[i]);
+                }
+                reversedText.Append(reversedWord + " ");
+            }
+            return reversedText.ToString().Trim();
+        }
+
+        static void Main(string[]  args) {
+            Console.WriteLine("Enter a paragraph of text:");
+            var paragraph = Console.ReadLine();
+            var summarizedText = SummarizeText(paragraph);
+            Console.WriteLine("Summarized text:");
+            Console.WriteLine(summarizedText);
+
+            int wordCount = CountWords(paragraph);
+            int sentenceCount = CountSentences(paragraph);
+            int characterCount = CountCharacters(paragraph);
+            Console.WriteLine("Number of words: " + wordCount);
+            Console.WriteLine("Number of sentences: " + sentenceCount);
+            Console.WriteLine("Number of characters: " + characterCount);
+            string longestWord = FindLongestWord(paragraph);
+            Console.WriteLine("Longest word: " + longestWord);
+            string reversedParagraph = ReverseWords(paragraph);
+            Console.WriteLine("Reversed paragraph: " + reversedParagraph);
+        }
+
+    }
+}
