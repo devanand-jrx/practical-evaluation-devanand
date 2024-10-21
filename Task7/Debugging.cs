7b3c5e81c434377cbf453229f5f9a61d2344309c
devanand-jrx <devanand.ravindran@edstem.com>
2024-10-21 15:50:18 +0530

    debugging

---
+++ Task7/Debugging.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalEvaluationDevanand.Task7
+{
+    internal class Debugging
+    {
+        public class Calculator
+        {
+            public int Divide(int a, int b)
+            {
+                if (b == 0)
+                {
+                    Console.WriteLine("Divider cannot be zero.");
+                    return 0;
+                }
+                return a / b;
+            }
+
+            public void ProcessNumbers(int[] numbers)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    var result = Divide(numbers[i], 2);
+                    Console.WriteLine("Result: " + result);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var processor = new Calculator();
+            int[] numbers = { 0, 20, 30, 40, 50 };
+            processor.ProcessNumbers(numbers);
+        }
+    }
+}
