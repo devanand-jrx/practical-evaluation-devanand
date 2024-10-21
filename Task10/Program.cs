32d5f63e297e3105830169011380e53f505c4cdb
devanand-jrx <devanand.ravindran@edstem.com>
2024-10-21 17:24:24 +0530

    minor formating

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,25 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace PracticalEvaluationDevanand.Task10
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Shape circle = new Circle("Circle", 5);
-            Console.WriteLine(circle.GetName());
-            Console.WriteLine("Area: " + circle.GetArea());
-            Shape rectangle = new Rectangle("Rectangle", 4, 6);
-            Console.WriteLine(rectangle.GetName());
-            Console.WriteLine("Area: " + rectangle.GetArea());
-            Shape triangle = new Triangle("Triangle", 3, 4);
-            Console.WriteLine(triangle.GetName());
-            Console.WriteLine("Area: " + triangle.GetArea());
+//namespace PracticalEvaluationDevanand.Task10
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Shape circle = new Circle("Circle", 5);
+//            Console.WriteLine(circle.GetName());
+//            Console.WriteLine("Area: " + circle.GetArea());
+//            Shape rectangle = new Rectangle("Rectangle", 4, 6);
+//            Console.WriteLine(rectangle.GetName());
+//            Console.WriteLine("Area: " + rectangle.GetArea());
+//            Shape triangle = new Triangle("Triangle", 3, 4);
+//            Console.WriteLine(triangle.GetName());
+//            Console.WriteLine("Area: " + triangle.GetArea());
           
-        }
-    }
-}
+//        }
+//    }
+//}
---
+++ Task5/Event.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalEvaluationDevanand.Task5
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public DateTime StartDateTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime EndDateTime
+        {
+            get { return StartDateTime.Add(Duration); }
+        }
+        public TimeSpan TimeUntilEvent
+        {
+            get { return StartDateTime - DateTime.Now; }
+        }
+        public override string ToString()
+        {
+            return $"Event: {Name}, Start: {StartDateTime}, Duration: {Duration}, Ends: {EndDateTime}, Time Until Event: {TimeUntilEvent}";
+        }
+    }
+}
