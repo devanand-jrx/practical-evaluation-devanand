92dd24b7ee23c143488bc9320df00722a1caea5c
devanand-jrx <devanand.ravindran@edstem.com>
2024-10-21 13:02:29 +0530

    bank account

--- Employee/Employee.cs
+++ Employee/Employee.cs
@@ -1,54 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace PracticalEvaluationDevanand.Employee
-{
-    internal class Employee
-    {
-        static void Main(string[] args)
-        {
-            var grosspay = 0;
+//namespace PracticalEvaluationDevanand.Employee
+//{
+//    internal class Employee
+//    {
+//        static void Main(string[] args)
+//        {
+//            var grosspay = 0;
 
-            Console.WriteLine("Enter the hourly rate in Rs. ");
-            bool isRate = int.TryParse(Console.ReadLine(), out var rate);
-            if (!isRate)
-            {
-                Console.WriteLine("not a valid rate");
-            }
-            Console.WriteLine("Enter the hours you worked");
-            bool isHours = int.TryParse(Console.ReadLine(), out var hours);
-            if (!isHours)
-            {
-                Console.WriteLine("not a valid hour");
-            }
+//            Console.WriteLine("Enter the hourly rate in Rs. ");
+//            bool isRate = int.TryParse(Console.ReadLine(), out var rate);
+//            if (!isRate)
+//            {
+//                Console.WriteLine("not a valid rate");
+//            }
+//            Console.WriteLine("Enter the hours you worked");
+//            bool isHours = int.TryParse(Console.ReadLine(), out var hours);
+//            if (!isHours)
+//            {
+//                Console.WriteLine("not a valid hour");
+//            }
 
-            var pay = hours * rate;
+//            var pay = hours * rate;
 
-            if (hours > 0 && hours <= 40)
-            {
-                Console.WriteLine($"Total Gross Pay is Rs.{pay}");
-            }
-            else if (hours > 40)
-            {
-                var overHours = hours - 40;
-                pay = (int)(pay +  (overHours * rate * 0.5)); 
-                Console.WriteLine($"Great! you worked a lot, Total gross pay is Rs.{pay}");
-            }
+//            if (hours > 0 && hours <= 40)
+//            {
+//                Console.WriteLine($"Total Gross Pay is Rs.{pay}");
+//            }
+//            else if (hours > 40)
+//            {
+//                var overHours = hours - 40;
+//                pay = (int)(pay +  (overHours * rate * 0.5)); 
+//                Console.WriteLine($"Great! you worked a lot, Total gross pay is Rs.{pay}");
+//            }
 
-            var tax = (pay * 20) / 100;
+//            var tax = (pay * 20) / 100;
 
-            Console.WriteLine($"tax is {tax}");
+//            Console.WriteLine($"tax is {tax}");
 
-            var netPay = pay - tax;
-            Console.WriteLine($"net pay is {netPay}");
-
-
-        }
+//            var netPay = pay - tax;
+//            Console.WriteLine($"net pay is {netPay}");
 
 
+//        }
 
-    }
-}
+
+
+//    }
+//}
